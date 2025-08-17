54fd6db5b648fc71145b741f3f3457967ca5e749
Sion0937 <2201879152@qq.com>
2025-08-17 15:00:19 +0800

    删除多余文件

--- LoveLoveSchoolDays_SharedConfigLibrary/SharedConfigLibrary.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using BepInEx.Configuration;
-namespace LoveLoveSchoolDays_SharedConfigLibrary
-{
-    public static class SharedConfigLibrary
-    {
-        public static ConfigEntry<bool> God1Config { get; set; }
-    }
-}
